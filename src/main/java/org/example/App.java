ecfae3421f4f28939436233e0cc2663e0e0ca811
Fagan <gafulov@yandex.ru>
2024-12-20 22:23:47 +0300

    Great First Maven Project V.2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Салам World!" );
+        System.out.println("Изменил код для второй версии коммита");
     }
+
 }
